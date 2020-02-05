729dbe643700d8765357b4f52c12360cc819d6b7
Mohammed Rayyan Shaji <rayyanz95@gmail.com>
2020-02-04 21:21:31 -0600

    Achieved nutritonal properties for searched food item.

--- sample.js
+++ sample.js
@@ -2,6 +2,9 @@ function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
+    var selectedfoods;
+    var isLargeNumber;
+    var fooddetails;
   /*execute a function when someone writes in the text field:*/
   inp.addEventListener("input", function(e) {
       var a, b, i, val = this.value;
@@ -18,6 +21,7 @@ function autocomplete(inp, arr) {
       /*for each item in the array...*/
 
       var happylist;
+      
 
       $.ajaxSetup({
       method: 'GET',
@@ -26,7 +30,7 @@ function autocomplete(inp, arr) {
         'x-app-key': '9ff34a709f1d44f63aa59fee827713d1',
       }});
 
-       $.get('https://trackapi.nutritionix.com/v2/search/instant', {query: inp.value}, lstofresults)
+       $.get('https://trackapi.nutritionix.com/v2/search/instant', {query: inp.value , detailed: true}, lstofresults)
       // const searchFoods = async searchText => {
       // const res = await fetch('https://trackapi.nutritionix.com/v2/search/instant?query=milk'
       //   ,{
@@ -41,17 +45,20 @@ function autocomplete(inp, arr) {
       //const fooditems = await res.json();
       //console.log(fooditem.common);
       
-      function lstofresults(results){
+      function lstofresults(results) {
 
         const lstResults = results;
         
         happylist = lstResults; 
-        console.log(happylist.common);
+        //console.log(happylist.common);
+        
         var foodindex = happylist.common;
+          //var selectedfood;
             for (i = 0; i < foodindex.length; i++) {
                 /*check if the item starts with the same letters as the text field value:*/
+                //console.log(happylist.common[i]);
                 if (happylist.common[i].food_name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                  console.log('Matched! '+happylist.common[i].food_name);
+                  //console.log('Matched! '+happylist.common[i]);
                   /*create a DIV element for each matching element:*/
                   b = document.createElement("DIV");
                   /*make the matching letters bold:*/
@@ -59,19 +66,51 @@ function autocomplete(inp, arr) {
                   b.innerHTML += happylist.common[i].food_name.substr(val.length);
                   /*insert a input field that will hold the current array item's value:*/
                   b.innerHTML += "<input type='hidden' value='" + happylist.common[i].food_name + "'>";
+           
                   /*execute a function when someone clicks on the item value (DIV element):*/
-                  b.addEventListener("click", function(e) {
+                  b.addEventListener("click", function (e) {
                       /*insert the value for the autocomplete text field:*/
                       inp.value = this.getElementsByTagName("input")[0].value;
+                      selectedfoods = inp.value;
+                               
+                    isLargeNumber = foodindex.findIndex(function(item, i){
+                      return item.food_name === inp.value
+                    });
+                    //var ele = foodindex.findIndex(isLargeNumber);
+                    fooddetails = happylist.common[isLargeNumber];
+                    
+                      
+                      var body = {
+                          query: inp.value,
+                          timezone: "US/Eastern",
+                      };
+                      
+                      
+                      
+                      
+//                      $.post(' https://trackapi.nutritionix.com/v2/natural/nutrients', {data: JSON.stringify(body)})
+//                          .then(function(data) {
+//                    console.log( "Data Loaded: " + data.foods );
+//                    });
+                      
                       /*close the list of autocompleted values,
                       (or any other open lists of autocompleted values:*/
                       closeAllLists();
+                      //return selectedfoods;
+                      showresult();
+                    
                   });
                   a.appendChild(b);
                 }
+                
+                //return selectedfood;
 
             }
+          
       }
+      
+      //lstofresults();
+      
       
   });
   
@@ -127,6 +166,44 @@ function autocomplete(inp, arr) {
         }
       }
     }
+    function showresult(){
+        
+        console.log("selected food is : "+selectedfoods); 
+        console.log("index details of selected food " +fooddetails.full_nutrients[4].value); 
+        //if(document.getElementById("results") != null){
+        var k = document.getElementById("result");
+        k.innerHTML += "<p> You have chosen " +selectedfoods+ "</p>";
+        k.innerHTML += "<img src = " +fooddetails.photo.thumb+ "> </>";
+        k.innerHTML += "<p> Serving Weight in gm " +fooddetails.serving_weight_grams+ "</p>"
+        
+        + "<p>Energy Kcal " +fooddetails.full_nutrients[4].value+ "</p>";
+        
+//                $.ajaxSetup({
+//                      method: 'POST',
+//                          async: true,
+//                          crossDomain: true,
+//                          url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+//                          //data: JSON.stringify({query:selectedfoods, timezone: "US/Eastern"}),
+//                        headers: {
+//                        'x-app-id':'36e650cf',
+//                        'x-app-key': '9ff34a709f1d44f63aa59fee827713d1',
+//                            'x-remote-user-id': 0,
+//                        'Content-Type': "application/json;",
+//                            'Accept':"application/json;",
+//                            
+//                      },
+//                      dataType: "json",
+//                      success: function(data)
+//                        {
+//                          console.log("data "+data);
+//                        },
+//                          error : function(){
+//                              console.log("Error...")
+//                          }
+//                      });
+//        $.get("https://trackapi.nutritionix.com/v2/natural/nutrients", JSON.stringify({query: selectedfoods, timezone: "US/Eastern"}));
+                
+}
 /*execute a function when someone clicks in the document:*/
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
@@ -134,4 +211,10 @@ document.addEventListener("click", function (e) {
 }
 
 
+
+
 autocomplete(document.getElementById("myInput"), ['a','b','c','cat','caterpillar']);
+//var bla = selectedfoods;
+
+
+
